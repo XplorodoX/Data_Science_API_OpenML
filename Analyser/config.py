7f45db149c9559ac37ede4b3b131893d87b9262a
Florian Merlau <florian.merlau@gmail.com>
2024-02-26 11:57:40 +0100

    Update, add Initial Cache

--- Analyser/config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-# hier soll später für den Analyzer der Datenset ID übergeben werden. 
-# wenn dataset_id existiert, wird dieser benutzt, ansonsten wird lokal bisher eine testdatei für lücken im datenset genutzt
-
-dataset_id = 1493
